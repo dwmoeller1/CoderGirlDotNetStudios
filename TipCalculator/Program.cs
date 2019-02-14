e545e20cdc511ef708aa83bd86f18128150c2dae
dwmoeller1 <dwmoeller1@aol.com>
2019-02-13 19:51:22 -0600

    Create tip calculator

--- TipCalculator/Program.cs
+++ TipCalculator/Program.cs
@@ -6,7 +6,35 @@ namespace TipCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What was your bill amount?");
+            string billInput = Console.ReadLine();
 
+            billInput = billInput.Trim().Trim('$');
+            double bill;
+            if (!double.TryParse(billInput, out bill))
+            {
+                Console.WriteLine($"The value {billInput} is not a valid bill amount.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("What percent would you like to tip?");
+            string tipPercentInput = Console.ReadLine();
+
+            tipPercentInput = tipPercentInput.Trim().Trim('%');
+            double tipPercent;
+            if (!double.TryParse(tipPercentInput, out tipPercent))
+            {
+                Console.WriteLine($"The value {tipPercentInput} is not a valid tip.");
+            }
+
+            double tipAmount = Math.Round(tipPercent * bill / 100, 2);
+            double totalBill = Math.Round(bill + tipAmount, 2);
+
+            Console.WriteLine($"You tip amount is ${tipAmount}.");
+            Console.WriteLine($"Your total bill ${totalBill}.");
+
+            Console.ReadLine();
         }
     }
 }
