9c0eb0287fb4d992988cb9677f1d0aac712a18f7
dwmoeller1 <dwmoeller1@aol.com>
2019-02-25 18:06:34 -0600

    Simple while loops for parsing input.

--- TipCalculator/Program.cs
+++ TipCalculator/Program.cs
@@ -8,28 +8,36 @@ namespace TipCalculator
         {
             double bill = 0;
             double tipPercent = 0;
-            bool goodInput = false;
-            while (goodInput)
+            //This is a typical way of doing a while loop when there is only one logical way to exit the loop 
+            //(ie. getting a parsable input), and you are ok with looping forever till that condition is met.
+            //Notice that I did a seperate loop for each entry. Why that instead of one loop for both entries?
+            while (true)
             {
                 Console.WriteLine("What was your bill amount?");
                 string billInput = Console.ReadLine();
 
                 billInput = billInput.Trim().Trim('$');
 
-                if (!double.TryParse(billInput, out bill))
-                {
-                    Console.WriteLine($"The value {billInput} is not a valid bill amount.");
-                    Console.ReadLine();
-                    return;
+                if (double.TryParse(billInput, out bill))
+                {                    
+                    break;
                 }
 
+                Console.WriteLine($"The value {billInput} is not a valid bill amount. Please try again.");
+            }
+
+            while (true)
+            { 
                 Console.WriteLine("What percent would you like to tip?");
                 string tipPercentInput = Console.ReadLine();
                 tipPercentInput = tipPercentInput.Trim().Trim('%');
-                if (!double.TryParse(tipPercentInput, out tipPercent))
+
+                if (double.TryParse(tipPercentInput, out tipPercent))
                 {
-                    Console.WriteLine($"The value {tipPercentInput} is not a valid tip.");
+                    break;
                 }
+
+                Console.WriteLine($"The value {tipPercentInput} is not a valid tip. Please try again.");
             }
 
             double tipAmount = Math.Round(tipPercent * bill / 100, 2);
