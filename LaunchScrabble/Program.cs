e6ad405e1a576ff489e488198890048ac8d99db8
dwmoeller1 <dwmoeller1@aol.com>
2019-03-08 00:21:19 -0600

    Added Week 7 studio and in class work

---
+++ LaunchScrabble/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace LaunchScrabble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a your list of words. Separate each with a comma.");
+            string userInput = Console.ReadLine();
+            string[] wordArray = userInput.Trim().ToLower().Split(",");
+            int score = GetScore(wordArray);
+
+            Console.WriteLine($"You scored {score} points");
+
+            Console.ReadLine();
+        }
+
+        public static int GetScore(string[] wordArray)
+        {
+            int score = 0;
+            for(int i = 0; i< wordArray.Length; i++)
+            {
+                string wordToScore = wordArray[i].Trim();
+                if(wordToScore[0] == 'q')
+                {
+                    if (wordToScore.Length < 2 || wordToScore[1] != 'u')
+                        return 0;
+                    else
+                        score += 10;
+                }
+                else
+                {
+                    score++;
+                }
+            }
+
+            return score;
+        }
+    }
+}
---
+++ SplitAndJoin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SplitAndJoin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 33, 3, 6, 2 };
+            string output = String.Join(",", numbers);
+            Console.WriteLine(output);
+
+            string someNumberString = "23,64,33,9,101";
+            string[] someStringArrayOfNumbers = someNumberString.Split(',');
+            int[] someArrayOfInts = new int[someNumberString.Length];
+            for (int i = 0; i < someStringArrayOfNumbers.Length; i++)
+            {
+                int number = Convert.ToInt32(someStringArrayOfNumbers[i]);
+                someArrayOfInts[i] = number;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
